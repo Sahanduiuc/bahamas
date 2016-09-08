52f232b23519fbb73b495806aca3374fb846bf5d
Shamitha Ranasinghe <shamitha4@hotmail.com>
2016-09-08 18:12:17 +0100

    read JSON strategy grammar

--- c#/bahamas_system/Bahamas_System/GE/GESystem.cs
+++ c#/bahamas_system/Bahamas_System/GE/GESystem.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using bahamas_system.Bahamas_System.GE.Operators;
 using bahamas_system.Bahamas_System.GE.Operators.Functions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace bahamas_system.Bahamas_System.GE
 {
@@ -19,6 +22,7 @@ namespace bahamas_system.Bahamas_System.GE
 
         private List<Operator> generationResults;
         private GEFactory factory;
+        private Dictionary<string, List<LinkedList<string>>> geGrammar;
 
         public GESystem(double mutationRate, double crossOverRate,
             UInt16 maxPoolSize,UInt16 maxGenerations, UInt16 maxTreeSize)
@@ -30,10 +34,35 @@ namespace bahamas_system.Bahamas_System.GE
             this.MaxTreeSize = maxTreeSize;
 
             factory = new GEFactory();
+            geGrammar = new Dictionary<string, List<LinkedList<string>>>();
         }
 
         public void Initiate()
         {
+            //Read Strategy Grammar
+            using (StreamReader reader = new StreamReader("teststrat.json"))
+            {
+                string readString = reader.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(readString);
+                foreach (JProperty item in array)
+                {
+                    List<LinkedList<string>> optionsList = new List<LinkedList<string>>();
+                    foreach (var options in item.Value)
+                    {
+                        LinkedList<string> operatorList = new LinkedList<string>();
+                        foreach (var op in options)
+                        {
+                            operatorList.AddLast(op.ToString());
+                        }
+                        optionsList.Add(operatorList);
+                    }
+                    geGrammar.Add(item.Name,optionsList);
+                }
+            }
+
+            //TODO Generate random strats
+
+
             Random rnd = new Random();
             for (int iteration = 0; iteration < MaxPoolSize; iteration++)
             {
