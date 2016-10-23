dd7fcd9f4c1d4910e04102277bb05c2493759cb3
Shamitha Ranasinghe <shamitha4@hotmail.com>
2016-10-23 01:37:07 +0100

    Add c++ proj

---
+++ BahamasSystem/src/BahamasSystem.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BahamasSystem.cpp
+// Author      : Shamitha R
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Testing" << endl; // prints Testing
+	return 0;
+}
