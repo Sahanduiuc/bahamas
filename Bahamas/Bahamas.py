12f64390f3b3a7a387189f4e37851234ef5a6f04
Shamitha Ranasinghe <shamitha4@hotmail.com>
2018-04-03 23:52:51 +0100

    clean

--- Bahamas/Bahamas.py
+++
@@ -1,8 +0,0 @@
-import optionchain
-import spread
-
-cManager  = optionchain.OptionChainManager()
-cManager.addNewInstrument("RUT")
-
-sConstructor = spread.SpreadConstructor(cManager)
-sConstructor.createWidthValueSpread("RUT","19/02/2016","Credit","Put",30,3,"04/01/2016")
