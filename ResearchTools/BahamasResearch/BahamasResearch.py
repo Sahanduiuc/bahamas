e89ddcc8710c5379e91915b953e24f9c5f6e3c9d
Shamitha Ranasinghe <shamitha4@hotmail.com>
2018-07-12 09:51:59 +0100

    refactor backtest viewer

--- ResearchTools/BahamasResearch/BahamasResearch.py
+++ ResearchTools/BahamasResearch/BahamasResearch.py
@@ -5,9 +5,11 @@ import pandas as pd
 import numpy as np
 import json
 
+from BahamasResearch import TimeSeries as ts
+
 output_notebook()
 
-def RenderBacktest():
+def render_backtest():
     j = json.load(open('D:\\Backtest_Results\\131746912925578610_NZBWB.json'))
     seriesDates = []
     regTValues = []
@@ -17,10 +19,7 @@ def RenderBacktest():
         seriesDates.append(datetime.strptime(tuple['Item1'], "%d/%m/%Y %H:%M:%S"))
         regTValues.append(tuple['Item2'])
     
-    regTPlt = figure(plot_width = 950, plot_height = 375, title = 'NZBWB RegTRisk',
-            x_axis_label = 'Date', y_axis_label = 'RegTRisk',
-            x_axis_type="datetime")
-    regTPlt.circle(seriesDates, regTValues, line_width=1)
-    show(regTPlt)
+    regTSeries = ts.TimeSeries('NZBWB RegTRisk', 950, 375, 'Date', 'RegTRisk', seriesDates, regTValues)
+    regTSeries.render_plot()
 
-RenderBacktest()
+render_backtest()
--- ResearchTools/BahamasResearch/TimeSeries.py
+++ ResearchTools/BahamasResearch/TimeSeries.py
@@ -1,4 +1,13 @@
-class TimeSeries(object):
-    def __init__(self, title, width, height, xLabel, yLabel, xVals, yVals):
-        pass
+from bokeh.plotting import figure, output_file, show
 
+class TimeSeries(object):
+    
+    def __init__(self, title, width, height, xLabel, yLabel, xVals, yVals):
+        self.series_plot = figure(plot_width = width, plot_height = height, 
+            title = title,
+            x_axis_label = xLabel, y_axis_label = yLabel,
+            x_axis_type="datetime")
+        self.series_plot.circle(xVals, yVals, line_width=1)
+
+    def render_plot(self):
+        show(self.series_plot)
